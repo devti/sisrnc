df39cb04b9eea364dbf61ea18cc4043d4c895b94
Lucas Miranda <lucasmiranda_1@hotmail.com>
2015-02-19 14:51:20 -0300

    comitando tudo

---
+++ src/rnc/sismedicao/gui/LoginGui.java
@@ -0,0 +1,102 @@
+package rnc.sismedicao.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.JTextField;
+
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+
+public class LoginGui extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField TF_Login;
+	private JPasswordField PF_Senha;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginGui frame = new LoginGui();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginGui() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 260, 351);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblLogin = new JLabel("LOGIN");
+		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblLogin.setBounds(22, 107, 45, 14);
+		contentPane.add(lblLogin);
+		
+		TF_Login = new JTextField();
+		TF_Login.setBounds(22, 126, 200, 20);
+		contentPane.add(TF_Login);
+		TF_Login.setColumns(10);
+		
+		JLabel lblSenha = new JLabel("SENHA");
+		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblSenha.setBounds(22, 160, 45, 14);
+		contentPane.add(lblSenha);
+		
+		PF_Senha = new JPasswordField();
+		PF_Senha.setBounds(22, 176, 200, 20);
+		contentPane.add(PF_Senha);
+		
+		JButton BT_Confirmar = new JButton("LOGIN");
+		BT_Confirmar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		BT_Confirmar.setBounds(22, 207, 86, 23);
+		contentPane.add(BT_Confirmar);
+		
+		JButton BT_Cancelar = new JButton("SAIR");
+		BT_Cancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		BT_Cancelar.setBounds(124, 207, 98, 23);
+		contentPane.add(BT_Cancelar);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(LoginGui.class.getResource("/rnc/sismedicao/gui/img/MARCA-TI.png")));
+		label.setBounds(22, 32, 200, 48);
+		contentPane.add(label);
+	}
+}
--- src/rnc/sismedicao/model/dao/UsuarioDAO.java
+++ src/rnc/sismedicao/model/dao/UsuarioDAO.java
@@ -10,48 +10,53 @@ import rnc.sismedicao.model.beans.Usuario;
 import rnc.sismedicao.model.util.Conexao;
 
 public class UsuarioDAO {
-	
-	
+
 	public UsuarioDAO() {
-		// TODO Auto-generated constructor stub
+		
 	}
-	
-	public int insertUsuario(Usuario usuario){
-		
-		if(JOptionPane.showConfirmDialog(null, "tem certeza que quer cadastrar o usuário: "+usuario.getLogin()+"?"
-										 , "Confirmação de cadastro"
-										 , JOptionPane.YES_NO_OPTION) == JOptionPane.OK_OPTION){
-			
-		String query = "INSERT INTO USUARIO(LOGIN, SENHA) VALUES (?, ?, ?) ";
-		
-		
+
+	public int insertUsuario(Usuario usuario) throws Exception {
+
+		if (JOptionPane.showConfirmDialog(
+				null,
+				"tem certeza que quer cadastrar o usuário: "
+						+ usuario.getLogin() + "?", "Confirmação de cadastro",
+				JOptionPane.YES_NO_OPTION) == JOptionPane.OK_OPTION) {
+
+			String query = "INSERT INTO USUARIO(LOGIN, SENHA) VALUES (?, ?, ?) ";
+
 			try {
 				int i = 0;
 				ResultSet resultSet = null;
-				PreparedStatement preparedStatement = Conexao.getConnection().prepareStatement(query);
+				PreparedStatement preparedStatement = Conexao.getConnection()
+						.prepareStatement(query);
 				preparedStatement.setString(++i, usuario.getLogin());
 				preparedStatement.setString(++i, usuario.getSenha());
 				preparedStatement.setInt(++i, usuario.getCodPessoa());
-				
+
 				preparedStatement.execute();
-				
+
 				Conexao.getConnection().commit();
-				
+
 				resultSet = preparedStatement.getGeneratedKeys();
-				
-				if(resultSet.next()){
+
+				if (resultSet.next()) {
 					usuario.setCodUsuario(resultSet.getInt(1));
-					
+
 				}
-				
-				JOptionPane.showMessageDialog(null, "Usuário: "+usuario.getLogin()+" cadastrado com sucesso", "Cadastrado com sucesso", JOptionPane.INFORMATION_MESSAGE);
+
+				JOptionPane.showMessageDialog(null,
+						"Usuário: " + usuario.getLogin()
+								+ " cadastrado com sucesso",
+						"Cadastrado com sucesso",
+						JOptionPane.INFORMATION_MESSAGE);
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		
+
 		}
 		return usuario.getCodUsuario();
 	}
-	
+
 }
