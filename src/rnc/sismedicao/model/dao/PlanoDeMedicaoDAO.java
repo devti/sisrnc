d884b61cf9a596c1fbcdd49fb9db954edc6b90ec
raphaellorena <raphael@tvasabranca.com.br>
2015-05-04 16:04:52 -0300

    Plano de manutecao

--- src/rnc/sismedicao/model/dao/PlanoDeMedicaoDAO.java
+++ src/rnc/sismedicao/model/dao/PlanoDeMedicaoDAO.java
@@ -1,5 +1,27 @@
 package rnc.sismedicao.model.dao;
 
-public class PlanoDeMedicaoDAO {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import rnc.sismedicao.controller.exception.RepositorioException;
+import rnc.sismedicao.model.beans.*;
+import rnc.sismedicao.model.interfacesDao.*;
+import rnc.sismedicao.model.util.Conexao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class PlanoDeMedicaoDAO implements IRepositorioPlanoDeMedicao{
+
+	public PlanoDeMedicaoDAO (IRepositorioPlanoDeMedicao repositorioPlanoDeMedicao){
+		
+	}
+	
+	public void inserir (PlanoDeMedicao planoDeMedicao){
+		
+	}
+	
 }
